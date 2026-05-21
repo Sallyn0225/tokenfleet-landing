1caf0c45cdb4f85fd314fbd9410402f354dd69c0
Sallyn <Sallyn0225@users.noreply.github.com>
2026-05-21 15:21:38 +0800

    build: add react and tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://tokenfleet.cn',
   trailingSlash: 'never',
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   build: {
     inlineStylesheets: 'auto',
     assets: '_assets',
